07b1bf9475d48a125b5f661284bebbd3e0690007
Tomoya Hirano <tomoya@cookpad.com>
2016-03-22 15:56:38 +0100

    Use gsub

--- spec/arabic_normalizer_spec.rb
+++ spec/arabic_normalizer_spec.rb
@@ -7,81 +7,81 @@ describe ArabicNormalizer do
 
   it "Normalizer should replace ALEF_HAMZA_ABOVE by ALEF" do
     test = "أسبوع"
-    expect(ArabicNormalizer::normalize(test)).to eq "اسبوع"
+    expect(ArabicNormalizer.normalize(test)).to eq "اسبوع"
   end
 
   it "Normalizer should replace ALEF_HAMZA_BELOW by ALEF" do
     test = "إنسان"
-    expect(ArabicNormalizer::normalize(test)).to eq "انسان"
+    expect(ArabicNormalizer.normalize(test)).to eq "انسان"
   end
 
   it "Normalizer should replace ALEF_MADDA by ALEF" do
     test = "الآن"
-    expect(ArabicNormalizer::normalize(test)).to eq "الان"
+    expect(ArabicNormalizer.normalize(test)).to eq "الان"
   end
 
   it "Normalizer should replace WAW_HAMZA by WAW" do
     test = "مسؤول"
-    expect(ArabicNormalizer::normalize(test)).to eq "مسوول"
+    expect(ArabicNormalizer.normalize(test)).to eq "مسوول"
   end
 
   it "Normalizer should replace YEH_HAMZA by YEH" do
     test = "شيئ"
-    expect(ArabicNormalizer::normalize(test)).to eq "شيي"
+    expect(ArabicNormalizer.normalize(test)).to eq "شيي"
   end
 
   it "Normalizer should replace DOTLESS_YEH by YEH" do
     test = "صدى"
-    expect(ArabicNormalizer::normalize(test)).to eq "صدي"
+    expect(ArabicNormalizer.normalize(test)).to eq "صدي"
   end
 
   it "Normalizer should replace TEH_MARBOUTA by HEH" do
     test = "عودة"
-    expect(ArabicNormalizer::normalize(test)).to eq "عوده"
+    expect(ArabicNormalizer.normalize(test)).to eq "عوده"
   end
 
   it "Normalizer should remove TATWEEL" do
     test = "قــــــال"
-    expect(ArabicNormalizer::normalize(test)).to eq "قال"
+    expect(ArabicNormalizer.normalize(test)).to eq "قال"
   end
 
   it "Normalizer should remove FATHATAN" do
     test = "شهراً"
-    expect(ArabicNormalizer::normalize(test)).to eq "شهرا"
+    expect(ArabicNormalizer.normalize(test)).to eq "شهرا"
   end
 
   it "Normalizer should remove DAMMATAN" do
     test = "مكتبٌ"
-    expect(ArabicNormalizer::normalize(test)).to eq "مكتب"
+    expect(ArabicNormalizer.normalize(test)).to eq "مكتب"
   end
 
   it "Normalizer should remove KASRATAN" do
     test = "شهرٍ"
-    expect(ArabicNormalizer::normalize(test)).to eq "شهر"
+    expect(ArabicNormalizer.normalize(test)).to eq "شهر"
   end
 
   it "Normalizer should remove FATHA" do
     test = "ولدَ"
-    expect(ArabicNormalizer::normalize(test)).to eq "ولد"
+    expect(ArabicNormalizer.normalize(test)).to eq "ولد"
   end
 
   it "Normalizer should remove DAMMA" do
     test = "ولدُ"
-    expect(ArabicNormalizer::normalize(test)).to eq "ولد"
+    expect(ArabicNormalizer.normalize(test)).to eq "ولد"
   end
 
   it "Normalizer should remove KASRA" do
     test = "شمسِ"
-    expect(ArabicNormalizer::normalize(test)).to eq "شمس"
+    expect(ArabicNormalizer.normalize(test)).to eq "شمس"
   end
 
   it "Normalizer should remove SHADDA" do
     test = "فعّال"
-    expect(ArabicNormalizer::normalize(test)).to eq "فعال"
+    expect(ArabicNormalizer.normalize(test)).to eq "فعال"
   end
 
   it "Normalizer should remove SUKUN" do
     test = "كسْر"
-    expect(ArabicNormalizer::normalize(test)).to eq "كسر"
+    expect(ArabicNormalizer.normalize(test)).to eq "كسر"
   end
 end
